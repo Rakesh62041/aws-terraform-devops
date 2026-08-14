9371e4716fa63ececeb39a42631f23aeadd52674
Rakesh Sharma <rakesharma620@zohomail.in>
2026-08-14 11:11:22 +0000

    Add EC2 security group

---
+++ terraform/ec2_security_group.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "ec2" {
+  name        = "aws-terraform-devops-ec2-sg"
+  description = "Security group for EC2 instances"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "Allow HTTP traffic from ALB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  ingress {
+    description = "Allow SSH from anywhere for project testing"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "aws-terraform-devops-ec2-sg"
+  }
+}
